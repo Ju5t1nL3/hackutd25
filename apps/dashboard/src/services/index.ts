090f4473b1085379b325260b5601898087ff061d
Ju5t1nL3 <justinle2006@gmail.com>
2025-11-09 05:26:07 -0600

    Update calls api name

--- apps/dashboard/src/services/index.ts
+++ apps/dashboard/src/services/index.ts
@@ -1,20 +1,21 @@
 // src/services/index.ts
 
-export * from './api-config'
-export * from './call-logs-api'
-export * from './customers-api'
-export * from './dashboard-api'
-export * from './properties-api'
+export * from "./api-config";
+export * from "./call-logs-api";
+export * from "./customers-api";
+export * from "./dashboard-api";
+export * from "./properties-api";
 
 // Exporting the initialized service instances
-import { callLogsApiService } from './call-logs-api'
-import { customersApiService } from './customers-api'
-import { dashboardApiService } from './dashboard-api'
-import { propertiesApiService } from './properties-api'
+import { getCallLogs } from "./call-logs-api";
+import { customersApiService } from "./customers-api";
+import { dashboardApiService } from "./dashboard-api";
+import { propertiesApiService } from "./properties-api";
 
 export const services = {
-  callLogs: callLogsApiService,
+  callLogs: { getCallLogs },
   customers: customersApiService,
   dashboard: dashboardApiService,
   properties: propertiesApiService,
-}
+};
+
