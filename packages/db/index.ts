60c70b4f4fbdf6441c7dadcf51eb7ba7ed0b2ff5
Ju5t1nL3 <justinle2006@gmail.com>
2025-11-09 05:20:05 -0600

    Modularize PrismaClient

---
+++ packages/db/index.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
+// This prevents Next.js hot-reloading from creating too many
+// new PrismaClient instances in development.
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+// We also re-export the generated types
+export * from '@prisma/client';
